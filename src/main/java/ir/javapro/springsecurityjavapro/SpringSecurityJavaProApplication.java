42fd85570c5d21a9968b1619519ed319fe2c6ec2
mahdikarimi422@gmail.com <mahdikarimi422@gmail.com>
2024-06-27 11:56:09 +0430

    create project

---
+++ src/main/java/ir/javapro/springsecurityjavapro/SpringSecurityJavaProApplication.java
@@ -0,0 +1,13 @@
+package ir.javapro.springsecurityjavapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJavaProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJavaProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/javapro/springsecurityjavapro/SpringSecurityJavaProApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.javapro.springsecurityjavapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJavaProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
